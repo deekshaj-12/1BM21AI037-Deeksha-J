afce74c014fe8c252759e830b681a5933593b4b7
Deeksha Jyanath <96129825+deekshaj-12@users.noreply.github.com>
2023-07-18 10:25:21 +0530

    Update kruskals-18.07.23.cpp

--- kruskals-18.07.23.cpp
+++ kruskals-18.07.23.cpp
@@ -5,80 +5,76 @@ using namespace std;
 class Graph {
 private:
     int V;
-    int** adjMatrix;
+    int adjMatrix[100][100];
 
 public:
-    void initialize(int numVertices) {
-        V = numVertices;
-        adjMatrix = new int*[V];
-        for (int i = 0; i < V; ++i) {
-            adjMatrix[i] = new int[V];
-            for (int j = 0; j < V; ++j) {
-                adjMatrix[i][j] = 0;
-            }
+    void initialize(int numVertices);
+    void addEdge(int src, int dest, int weight);
+    void KruskalMST();
+    void clear();
+};
+
+void Graph::initialize(int numVertices) {
+    V = numVertices;
+    for (int i = 0; i < V; ++i) {
+        for (int j = 0; j < V; ++j) {
+            adjMatrix[i][j] = 0;
         }
     }
+}
 
-    void addEdge(int src, int dest, int weight) {
-        adjMatrix[src][dest] = weight;
-        adjMatrix[dest][src] = weight;
+void Graph::addEdge(int src, int dest, int weight) {
+    adjMatrix[src][dest] = weight;
+    adjMatrix[dest][src] = weight;
+}
+
+void Graph::KruskalMST() {
+    bool visited[100];
+    pair<int, int> edges[100];
+    int totalWeight = 0;
+
+    for (int i = 0; i < V; ++i) {
+        visited[i] = false;
     }
 
-    void KruskalMST() {
-        bool* visited = new bool[V];
-        for (int i = 0; i < V; ++i) {
-            visited[i] = false;
-        }
+    visited[0] = true;
 
-        pair<int, int>* edges = new pair<int, int>[V];
-        int totalWeight = 0;
+    for (int i = 1; i < V; ++i) {
+        int minWeight = INT_MAX;
+        int minIndex = -1;
 
-        visited[0] = true;
-
-        for (int i = 1; i < V; ++i) {
-            int minWeight = INT_MAX;
-            int minIndex = -1;
-
-            for (int j = 0; j < V; ++j) {
-                if (visited[j]) {
-                    for (int k = 0; k < V; ++k) {
-                        if (!visited[k] && adjMatrix[j][k] != 0 && adjMatrix[j][k] < minWeight) {
-                            minWeight = adjMatrix[j][k];
-                            minIndex = k;
-                        }
+        for (int j = 0; j < V; ++j) {
+            if (visited[j]) {
+                for (int k = 0; k < V; ++k) {
+                    if (!visited[k] && adjMatrix[j][k] != 0 && adjMatrix[j][k] < minWeight) {
+                        minWeight = adjMatrix[j][k];
+                        minIndex = k;
                     }
                 }
             }
-
-            if (minIndex != -1) {
-                visited[minIndex] = true;
-                edges[i] = make_pair(minIndex, minWeight);
-                totalWeight += minWeight;
-            }
         }
 
-        cout << "Edges in the minimum spanning tree:\n";
-        for (int i = 1; i < V; ++i) {
-            cout << "0 - " << edges[i].first << " : " << edges[i].second << "\n";
+        if (minIndex != -1) {
+            visited[minIndex] = true;
+            edges[i] = make_pair(minIndex, minWeight);
+            totalWeight += minWeight;
         }
-
-        cout << "Total weight of the minimum spanning tree: " << totalWeight << endl;
-
-        delete[] visited;
-        delete[] edges;
     }
 
-    void clear() {
-        for (int i = 0; i < V; ++i) {
-            delete[] adjMatrix[i];
-        }
-        delete[] adjMatrix;
-        V = 0;
+    cout << "Edges in the minimum spanning tree:\n";
+    for (int i = 1; i < V; ++i) {
+        cout << "0 - " << edges[i].first << " : " << edges[i].second << "\n";
     }
-};
+
+    cout << "Total weight of the minimum spanning tree: " << totalWeight << endl;
+}
+
+void Graph::clear() {
+    V = 0;
+}
 
 int main() {
-    Graph graph;
+    Graph g;
 
     int numVertices, numEdges;
     cout << "Enter the number of vertices: ";
@@ -87,18 +83,18 @@ int main() {
     cout << "Enter the number of edges: ";
     cin >> numEdges;
 
-    graph.initialize(numVertices);
+    g.initialize(numVertices);
 
     cout << "Enter the edges (src, dest, weight):\n";
     for (int i = 0; i < numEdges; ++i) {
         int src, dest, weight;
         cin >> src >> dest >> weight;
-        graph.addEdge(src, dest, weight);
+        g.addEdge(src, dest, weight);
     }
 
-    graph.KruskalMST();
+    g.KruskalMST();
 
-    graph.clear();
+    g.clear();
 
     return 0;
 }
